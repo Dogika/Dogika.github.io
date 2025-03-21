d43a7013566a3aaf1631bbd5fb795b13af59844a
Dogika <59515291+Dogika@users.noreply.github.com>
2025-03-21 04:49:48 +0100

    Update math.js

--- math.js
+++ math.js
@@ -1,86 +1,88 @@
-function rotate(vx, vy, rx, ry) {
-    return [vx * rx - vy * ry, vx * ry + vy * rx];
-}
-
-function expDecay(a, b, decay, dt) {
-    return b + (a - b) * Math.exp(-decay * dt);
-}
-
-function dot2D(v1, v2) {
-    let [x1, y1] = v1;
-    let [x2, y2] = v2;
-    return x1 * x2 + y1 * y2;
-}
-
-function normalize(v) {
-    var inv_magnitude = 1 / Math.hypot(...v);
-    if (v.length == 2) return scale2(inv_magnitude, v);
-    if (v.length == 3) return scale3(inv_magnitude, v);
-}
-
-function setMagnitude(m, v) {
-    var new_magnitude = m / Math.hypot(...v);
-    if (v.length == 2) return scale2(new_magnitude, v);
-    if (v.length == 3) return scale3(new_magnitude, v);
-}
-
-function cross(v1, v2) {
-    var [x1, y1, z1] = v1;
-    var [x2, y2, z2] = v2;
+Vclass MathHelper {
+    static rotate(vx, vy, rx, ry) {
+        return [vx * rx - vy * ry, vx * ry + vy * rx];
+    }
     
-    return [y1 * z2 - z1 * y2, z1 * x2 - x1 * z2, x1 * y2 - y1 * x2];
-}
-
-function scale2(s, v1) {
-    var [x1, y1] = v1;
+    static expDecay(a, b, decay, dt) {
+        return b + (a - b) * Math.exp(-decay * dt);
+    }
     
-    return [s * x1, s * y1];
-}
-
-function scale3(s, v1) {
-    var [x1, y1, z1] = v1;
+    static dot2D(v1, v2) {
+        let [x1, y1] = v1;
+        let [x2, y2] = v2;
+        return x1 * x2 + y1 * y2;
+    }
     
-    return [s * x1, s * y1, s * z1];
-}
-
-function mult2(v1, v2) {
-    var [x1, y1] = v1;
-    var [x2, y2] = v2;
+    static normalize(v) {
+        let inv_magnitude = 1 / Math.hypot(...v);
+        if (v.length == 2) return this.scale2(inv_magnitude, v);
+        if (v.length == 3) return this.scale3(inv_magnitude, v);
+    }
     
-    return [x1 * x2, y1 * y2];
-}
-
-function mult3(v1, v2) {
-    var [x1, y1, z1] = v1;
-    var [x2, y2, z2] = v2;
+    static setMagnitude(m, v) {
+        let new_magnitude = m / Math.hypot(...v);
+        if (v.length == 2) return this.scale2(new_magnitude, v);
+        if (v.length == 3) return this.scale3(new_magnitude, v);
+    }
     
-    return [x1 * x2, y1 * y2, z1 * z2];
-}
-
-function add2(v1, v2) {
-    var [x1, y1] = v1;
-    var [x2, y2] = v2;
+    static cross(v1, v2) {
+        let [x1, y1, z1] = v1;
+        let [x2, y2, z2] = v2;
+        
+        return [y1 * z2 - z1 * y2, z1 * x2 - x1 * z2, x1 * y2 - y1 * x2];
+    }
     
-    return [x1 + x2, y1 + y2];
-}
-
-function add3(v1, v2) {
-    var [x1, y1, z1] = v1;
-    var [x2, y2, z2] = v2;
+    static scale2(s, v1) {
+        let [x1, y1] = v1;
+        
+        return [s * x1, s * y1];
+    }
     
-    return [x1 + x2, y1 + y2, z1 + z2];
-}
-
-function subtract2(v1, v2) {
-    var [x1, y1] = v1;
-    var [x2, y2] = v2;
+    static scale3(s, v1) {
+        let [x1, y1, z1] = v1;
+        
+        return [s * x1, s * y1, s * z1];
+    }
     
-    return [x1 - x2, y1 - y2];
-}
-
-function subtract3(v1, v2) {
-    var [x1, y1, z1] = v1;
-    var [x2, y2, z2] = v2;
+    static mult2(v1, v2) {
+        let [x1, y1] = v1;
+        let [x2, y2] = v2;
+        
+        return [x1 * x2, y1 * y2];
+    }
     
-    return [x1 - x2, y1 - y2, z1 - z2];
+    static mult3(v1, v2) {
+        let [x1, y1, z1] = v1;
+        let [x2, y2, z2] = v2;
+        
+        return [x1 * x2, y1 * y2, z1 * z2];
+    }
+    
+    static add2(v1, v2) {
+        let [x1, y1] = v1;
+        let [x2, y2] = v2;
+        
+        return [x1 + x2, y1 + y2];
+    }
+    
+    static add3(v1, v2) {
+        let [x1, y1, z1] = v1;
+        let [x2, y2, z2] = v2;
+        
+        return [x1 + x2, y1 + y2, z1 + z2];
+    }
+    
+    static subtract2(v1, v2) {
+        let [x1, y1] = v1;
+        let [x2, y2] = v2;
+        
+        return [x1 - x2, y1 - y2];
+    }
+    
+    static subtract3(v1, v2) {
+        let [x1, y1, z1] = v1;
+        let [x2, y2, z2] = v2;
+        
+        return [x1 - x2, y1 - y2, z1 - z2];
+    }
 }
