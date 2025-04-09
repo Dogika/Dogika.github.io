87ea79683018bed822e1d9fa906e24d51c021647
Dogika <59515291+Dogika@users.noreply.github.com>
2025-04-09 07:06:59 +0200

    Create blood.js

---
+++ blood.js
@@ -0,0 +1,25 @@
+function Blood(spawnTime, x, y, weaponName) {
+    this.x = x;
+    this.y = y;
+    this.weapon = g_weaponList[weaponName];
+    this.spawnTime = spawnTime;
+    this.sizeFactor = (Math.random() - 0.5) * 20;
+    
+    this.heal = function() {
+        let dx = g_player.x - this.x;
+        let dy = g_player.y - this.y;
+        
+        if (dx * dx + dy * dy > this.weapon.radius * this.weapon.radius) 
+            return;
+        
+        g_player.health = Math.min(
+            G_PLAYER_MAX_HEALTH, 
+            g_player.health + this.weapon.healing
+        );
+    }
+    
+    this.display = function() {
+        console.log(this.sizeFactor);
+        drawCircle(ctx, this.x + g_camera.x, this.y + g_camera.y,  Math.max(0, this.sizeFactor + G_BLOOD_MAX_RADIUS * (1 - (g_currentTime-this.spawnTime)/G_BLOOD_DURATION)), Color.RED, 0.4);
+    }
+}
