8d114eebac96a8245cfd236356d46b55fdb8d705
Dogika <59515291+Dogika@users.noreply.github.com>
2025-04-17 00:24:17 +0200

    Update enemyPattern.js

--- enemyPattern.js
+++ enemyPattern.js
@@ -1,3 +1,19 @@
+function Pattern(bulletType=null, petals=0, petalOffset=0, x_bullets=0, x_angle=0, z_bullets=0, z_difference=0, z_offset=0, showStart=0, showOffset=0, towardsPlayer=false) {
+    this.bulletType = bulletType; // allows for editing behavior list without affecting global type
+    this.petals = petals;
+    this.petalOffset = petalOffset;
+    this.x_bullets = x_bullets;
+    this.x_angle = x_angle;
+    this.z_bullets = z_bullets;
+    this.z_difference = z_difference;
+    this.z_offset = z_offset;
+    this.showStart = showStart;
+    this.showOffset = showOffset;
+    this.loopColorID;
+    this.hueShift;
+    this.towardsPlayer = towardsPlayer;
+}
+
 function firePattern(patternCopy, enemy_x, enemy_y) {
     let patternInstanceID = g_patternInstanceIDs++;
     g_patternInstances[patternInstanceID] = [];
