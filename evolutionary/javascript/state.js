ae9cd9386b8ed1aaf8855abb9d2944fd1afe1c36
Dogika <59515291+Dogika@users.noreply.github.com>
2025-08-18 05:49:35 -0400

    new changes

--- evolutionary/javascript/state.js
+++ evolutionary/javascript/state.js
@@ -8,119 +8,15 @@ let ctx = canvas.getContext("2d");
 
 let g_mouse = {};
 
-let g_buttonInstances = [];
-
 let g_currentTime;
 let g_previousTime;
 let g_timeline = [];
 let g_nextEvent_ptr;
 
-let g_beeInstances = [];
-
-let g_gameWindow = "audioCheck";
-
-let g_currentRoom;
-
-let g_shop;
-
-let g_ball;
-
-let g_pollen = 0;
-let g_honey = 0;
-
-let g_unlocks = {};
-let g_progress = {};
-
-let g_clickableObjects
-
-let g_currentEquipment = {};
-
-let g_currentBoots = "";
-let g_currentHarvester = "";
-let g_currentBelt = "";
-let g_currentBackpack = "Default Backpack";
-let g_currentArmorHead = "";
-let g_currentArmorLeft = "";
-let g_currentArmorRight = "";
+let g_window = "audioCheck";
 
 let g_keyboard = {};
 
-let g_image = {
-    flowerRed1: new Image(),
-    flowerRed2: new Image(),
-    flowerRed3: new Image(),
-    flowerRed4: new Image(),
-    flowerRed5: new Image(),
-    flowerBlue1: new Image(),
-    flowerBlue2: new Image(),
-    flowerBlue3: new Image(),
-    flowerBlue4: new Image(),
-    flowerBlue5: new Image(),
-    flowerWhite1: new Image(),
-    flowerWhite2: new Image(),
-    flowerWhite3: new Image(),
-    flowerWhite4: new Image(),
-    flowerWhite5: new Image(),
-    navigatorLeft: new Image(),
-    navigatorRight: new Image(),
-    navigatorUp: new Image(),
-    navigatorDown: new Image()
-}
-
-function calculateStats() {
-    
-}
-
-const g_rooms = {
-    firstRoom: new Room(
-        [new Navigator("up", "strawberryFields"), new Navigator("right", "storeRoom"), new Navigator("left", "shippingRoom")], 
-        undefined, 
-        new Field(g_screenWidth*0.5, g_screenHeight*0.54, 9, 14, g_screenScale*0.8, g_screenScale*0.8, [1, 0, 0, 1, 0, 0, 6, 2, 0], true)),
-    strawberryFields: new Room(
-        [new Navigator("down", "firstRoom")], 
-        undefined, 
-        new Field(g_screenWidth*0.5, g_screenHeight*0.54, 5, 16, g_screenScale*0.8, g_screenScale*0.8, [6, 2, 0, 1, 0 ,0, 1, 0, 0], true)),
-    storeRoom: new Room(
-        [new Navigator("left", "firstRoom")], 
-        new Shop("navigatorLeft", g_screenScale*1.5, g_screenScale*1, "down", [g_boots["Basic Boots"], g_boots["Basic Boots"], g_boots["Basic Boots"], g_boots["Basic Boots"]]), 
-        new Field(g_screenWidth*0.5, g_screenHeight*0.5, 9, 14, g_screenScale*0.8, g_screenScale*0.8, [1, 0, 0, 6, 2, 0, 1, 0, 0], true)),
-    shippingRoom: new Room(
-        [new Navigator("right", "firstRoom")], 
-        undefined, 
-        undefined,
-        undefined,
-        undefined,
-        [new NPC()])
-};
-
-g_collectPatterns = {};
-
 function initializeGameState() {
-    g_image["flowerBlue1"].src = "https://codehs.com/uploads/d03248d119f4e02ee6683575a849d805";
-    g_image["flowerBlue2"].src = "https://codehs.com/uploads/463c73775036984b9f7a679c49a80367";
-    g_image["flowerBlue3"].src = "https://codehs.com/uploads/c39ba644a533aaa4ee2f0627c9202061";
-    g_image["flowerBlue4"].src = "";
-    g_image["flowerBlue5"].src = "";
-    g_image["flowerRed1"].src = "https://codehs.com/uploads/283ad8753972268436e85bbc607a85a4";
-    g_image["flowerRed2"].src = "https://codehs.com/uploads/1479c8b47b825375d58a267578f4e3ab";
-    g_image["flowerRed3"].src = "https://codehs.com/uploads/e5fdd452a9f48e230a9a5c1482d9c56c";
-    g_image["flowerRed4"].src = "";
-    g_image["flowerRed5"].src = "";
-    g_image["flowerWhite1"].src = "https://codehs.com/uploads/466c2a9113b888d56317a774bb569226";
-    g_image["flowerWhite2"].src = "https://codehs.com/uploads/1f0763115ea4e23630153c1ee748634f";
-    g_image["flowerWhite3"].src = "https://codehs.com/uploads/70a43003be13fc8d54db1057f0d1fb21";
-    g_image["flowerWhite4"].src = "";
-    g_image["flowerWhite5"].src = "";
     
-    g_image["navigatorLeft"].src = "https://codehs.com/uploads/cbee1d855602ad4d4a2b4225781174cb";
-    g_image["navigatorRight"].src = "https://codehs.com/uploads/cbee1d855602ad4d4a2b4225781174cb";
-    g_image["navigatorUp"].src = "https://codehs.com/uploads/cbee1d855602ad4d4a2b4225781174cb";
-    g_image["navigatorDown"].src = "https://codehs.com/uploads/cbee1d855602ad4d4a2b4225781174cb";
-    
-    g_currentEquipment["backpack"] = g_backpacks["Basic Backpack"];
-    
-    g_currentRoom = g_rooms.firstRoom;
-    g_ball = {x: 0, y: 0, canClick: true};
-    g_ball.x = Math.random() * (g_currentRoom.field.tileWidth * g_currentRoom.field.grid[0].length - 2 * 32) + 32 + g_currentRoom.field.x;
-    g_ball.y = Math.random() * (g_currentRoom.field.tileHeight * g_currentRoom.field.grid.length - 2 * 32) + 32 + g_currentRoom.field.y;
 }
--- evolutionary/javascript/window.js
+++ evolutionary/javascript/window.js
@@ -14,81 +14,23 @@ function WindowAudioCheck() {
     }
 }
 
-function WindowTitle() {
-    ctx.fillStyle = "black";
-    ctx.font = "80pt Arial";
-    ctx.textAlign = "center";
-    ctx.textBaseline = "middle";
-    ctx.fillText("Dotty Clicker 2", 0.5*g_screenWidth, 0.25*g_screenHeight);
-    
-    g_buttonInstances = [];
-    
-    g_buttonInstances.push(new Button("Start", "30pt Arial", 0, 15, 120, 35, function() {
-            g_buttonInstances = [];
-            g_gameWindow = "game";
-    }));
-    
-    g_buttonInstances.push(new Button("Load", "30pt Arial", 0, -65, 120, 35, function() {
-            g_buttonInstances = [];
-            g_gameWindow = "mainMenu";
-    }));
-    
-    g_buttonInstances.push(new Button("Settings", "30pt Arial", 0, -145, 120, 35, function() {
-            g_buttonInstances = [];
-            g_gameWindow = "mainMenu";
-    }));
-    
-    for (let i = 0; i < g_buttonInstances.length; i++) {
-        let button = g_buttonInstances[i];
-        
-        let relativeMouse_x = g_mouse.x + button.x - 0.5*g_screenWidth;
-        let relativeMouse_y = g_mouse.y + button.y - 0.5*g_screenHeight;
-        let touchingMousePointer = relativeMouse_x > -button.hw
-                                    && relativeMouse_x < button.hw
-                                    && relativeMouse_y > -button.hh
-                                    && relativeMouse_y < button.hh;
-        if (touchingMousePointer && g_mouse.down) {
-            ctx.strokeStyle = "black";
-            ctx.lineWidth = 5;
-            ctx.strokeRect(0.5*g_screenWidth + button.x - button.hw, 0.5*g_screenHeight - button.y - button.hh + 3, button.hw + button.hw, button.hh + button.hh);
-            
-            ctx.fillStyle = "black";
-            ctx.font = button.font;
-            ctx.textAlign = "center";
-            ctx.textBaseline = "middle";
-            ctx.fillText(button.display, 0.5*g_screenWidth+button.x, 0.5*g_screenHeight-button.y + 3);
-        } else {
-            ctx.strokeStyle = "black";
-            ctx.lineWidth = 5;
-            ctx.globalAlpha = 0.5;
-            ctx.strokeRect(0.5*g_screenWidth + button.x - button.hw, 0.5*g_screenHeight - button.y - button.hh + 3, button.hw + button.hw, button.hh + button.hh);
-            ctx.globalAlpha = 1;
-            ctx.strokeRect(0.5*g_screenWidth + button.x - button.hw, 0.5*g_screenHeight - button.y - button.hh, button.hw + button.hw, button.hh + button.hh);
-            
-            ctx.fillStyle = "black";
-            ctx.font = button.font;
-            ctx.textAlign = "center";
-            ctx.textBaseline = "middle";
-            ctx.globalAlpha = 0.5;
-            ctx.fillText(button.display, 0.5*g_screenWidth+button.x, 0.5*g_screenHeight-button.y + 3);
-            ctx.globalAlpha = 1;
-            ctx.fillText(button.display, 0.5*g_screenWidth+button.x, 0.5*g_screenHeight-button.y);
-        }
-    }
+function WindowRun() {
+    drawBackground();
 }
 
-function Button(display, font, x, y, hw, hh, fn) {
-    this.display = display;
-    this.font = font;
-    this.fn = fn;
-    this.x = x;
-    this.y = y;
-    this.hw = hw;
-    this.hh = hh;
-    this.touching = function(x, y) {
-        return x > -this.hw
-            && x < this.hw
-            && y > -this.hh
-            && y < this.hh;
+function drawBackground() {
+    ctx.fillStyle = "white";
+    ctx.fillRect(0, 0, g_screenWidth, g_screenHeight);
+    
+    var pattern = ctx.createPattern(createPatternImage(), "repeat");
+    ctx.fillStyle = pattern;
+    ctx.fillRect(0, 0, g_screenWidth, g_screenHeight);
+
+    function createPatternImage() {
+        var ctx = document.createElement("canvas").getContext("2d");
+        ctx.canvas.width = ctx.canvas.height = 4;   // = size of pattern base
+        ctx.fillStyle = "#fff";
+        ctx.fillRect(0,0,1,1);
+        return ctx.canvas;                          // canvas can be used as image source
     }
 }
